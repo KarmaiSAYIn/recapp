f448d33d2808c9e09de7f19af6aa056da3bda395
KarmaiSAYIn <jonahrdragon@hotmail.com>
2021-05-11 13:24:29 -0600

    Facilitated easy compilation; init commit

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -arch x86_64 -std=c++17 -mmacosx-version-min=10.15 -Wall -framework OpenGL -framework GLUT -lpng olcExampleProgram.cpp -o output
--- olcExampleProgram.cpp
+++ olcExampleProgram.cpp
@@ -30,7 +30,7 @@ public:
 int main()
 {
 	Example demo;
-	if (demo.Construct(256, 240, 4, 4))
+	if (demo.Construct(1280 / 4, 720 / 4, 4, 4))
 		demo.Start();
 
 	return 0;
