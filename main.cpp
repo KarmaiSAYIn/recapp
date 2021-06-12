2fa3702cafdd616840e1fc00e78dcba7f0fbc976
KarmaiSAYIn <jonahrdragon@hotmail.com>
2021-06-12 14:48:22 -0600

    Fullscreen by default

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     Game theGame(gfx, wnd);
     gfx.SetGameObject(&theGame);
     wnd.SetGameObject(&theGame);
-	if (theGame.Construct(Graphics::ScreenWidth, Graphics::ScreenHeight, 1, 1))
+	if (theGame.Construct(Graphics::ScreenWidth, Graphics::ScreenHeight, 1, 1, true))
 		theGame.Start();
 
 	return 0;
